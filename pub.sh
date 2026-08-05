6997d369993958863a2b48b7ea361e7406c51942
laomu1988 <laomu1988@qq.com>
2026-08-05 12:32:33 +0800

    build: 优化构建产物，移除未使用的 react/recore，修复 pub.sh

--- pub.sh
+++ pub.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+set -e
 
-rm build/xswitch.css build/xswitch.js build/xswitch.js.map build/background.js.map build/forward.js.map
-rm xswitch.zip
+# 移除旧包（若存在）
+rm -f xswitch.zip
+
+# 当前构建已不生成 .map，保留一行兼容语句
+rm -f build/*.map
+
+# 打包扩展
 zip -r xswitch.zip build
+
+# 打开 Chrome Web Store 开发者后台
 open https://chrome.google.com/webstore/devconsole/g04040876578415358611/idkjhjggpffolpidfkikidcokdkdaogg/edit/package?hl=zh_CN
